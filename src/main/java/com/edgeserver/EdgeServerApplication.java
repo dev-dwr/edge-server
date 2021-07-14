c61520722263a0693480b74f6334ea2538b15bbe
Dawid.Rym <dawid.jscript@gmail.com>
2021-07-14 17:28:44 +0200

    Initial commit

---
+++ src/main/java/com/edgeserver/EdgeServerApplication.java
@@ -0,0 +1,15 @@
+package com.edgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EdgeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServerApplication.class, args);
+    }
+
+}
